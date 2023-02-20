a0738bd974b3054408e4df14f88d23a1c42af4f0
cgasnier2022 <clement.gasnier2022@campus-eni.fr>
2023-02-20 16:21:24 +0100

    premier controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/home', name: 'main_home')]
+    public function home(): Response
+    {
+        return $this->render("main/home.html.twig");
+    }
+
+    /**
+     * @route("/test", name="main-test")
+     */
+    public function test(): Response
+    {
+        return $this->render('main/test.html.twig');
+    }
+    }
+
